151e36313ce3e8b67b841d3915932ae029d009f9
Neil Stansbury <neil@neilstansbury.com>
2013-04-29 12:29:03 +0100

    More work on initial few story scenarios

--- features/Jet.IO.tests.js
+++ features/Jet.IO.tests.js
@@ -1,35 +1,54 @@
 
+/*
+	Apache <=> HTTP Service <=> ServiceController
+	
+	Service
+	Master
+		ServiceController
+			Worker
+				RequestController
+				RequestDispatcher
+					Slave
+						RequestListener
+						RequestHandler + Request
+							register
+							unregister
+							execute
+						
+	
+	SCENARIO("").
+	END();
 
-var onmessage = function(){}
+*/
 
-var EXPORTED_SYMBOLS = ["testOperation1", "testOperation2"];
+	
+SCENARIO("When a ServiceController is sent an unregistered request URI it returns a 404 response").
+END();
 
-var testOperation1 = {
-	resource : "test://operations/1",
-	
-	action : Jet.IO.OperationActions.Get,
-	
-	dispatchType : Jet.IO.OperationDispatchTypes.Slave,
-	
-	register : function(){},
-	
-	dispatch : function(){
-		
-	}
-	
-}
+SCENARIO("When a RequestController is created it can specifiy the number of RequestDispatchers to create").
+END();
 
-var testOperation2 = {
-	resource : "test://operations/2",
-	
-	action : Jet.IO.OperationActions.Get,
-	
-	dispatchType : Jet.IO.OperationDispatchTypes.Slave,
-	
-	register : function(){},
-	
-	dispatch : function(){
-		
-	}
-	
-}
+SCENARIO("When a RequestDispatcher is created it can specifiy the slave script to load").
+END();
+
+SCENARIO("When a RequestListener is sent a put request it can register all the RequestHandlers in the slave script").
+END();
+
+SCENARIO("When a RequestListener is updated successfully it returns a 200 response").
+END();
+
+SCENARIO("When a RequestListener is NOT updated successfully it returns a 500 response").
+END();
+
+SCENARIO("When a RequestListener is sent a request it executes the correct RequestHandler").
+END();
+
+SCENARIO("When a RequestListener is sent a get request it can return a list of all the RequestHandlers registered").
+END();
+
+SCENARIO("When a RequestController receives a request for a Master dispatch it is executed by the ServiceController").
+END();
+
+SCENARIO("When a RequestHandler is NOT executed successfully it returns a 500 response").
+END();
+
